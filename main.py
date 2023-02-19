25145f73db17c2b68fcd8f44b014785042a982ac
Shivani Sharma <shivanisharma1496@gmail.com>
2023-02-19 02:26:48 -0500

    Changes from new branch

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+def branch():
+    print("Added new branch")
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +13,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    branch()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
