559ed5766ce21e406e99429dcedfbd942df21444
Shivani Sharma <shivanisharma1496@gmail.com>
2023-02-19 02:34:17 -0500

    Added new branch changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-def branch():
+def new_branch():
     print("Added new branch")
 
 def print_hi(name):
@@ -13,6 +13,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    branch()
+    new_branch()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
